b700306625997468a93243cc410c223f1609de17
Nemanja3214 <nemanja.majstorovic3214@gmail.com>
2024-01-09 23:21:14 +0100

    Adjusted house a bit

--- Sablon/Man.cpp
+++ Sablon/Man.cpp
@@ -9,11 +9,12 @@ Man::Man(Shader& shader, glm::mat4 view, glm::mat4 projection)
     shader.SetUniform3f("uLightColor", glm::vec3(1, 1, 1));
 
     this->model = glm::mat4(1.0f);
+    model = glm::scale(model, glm::vec3(0.25f, 0.45f, 0.25f));
     model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     //model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    model = glm::scale(model, glm::vec3(0.25f, 0.25f, 0.25f));
-    model = glm::translate(model, glm::vec3(-1.7f, 2.5f, -2.0f));
+   
+    model = glm::translate(model, glm::vec3(-1.7f, 2.25f, -1.75f));
 
     shader.SetUniformMat4f("uM", model);
 
